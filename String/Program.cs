6714c8b857427eabb6d0c753d23ca9d4766dd6a5
Rachel Pizane <rachel.pzm@gmail.com>
2025-02-12 22:03:15 -0300

    aprendendo como manipular datas e string

---
+++ String/Program.cs
@@ -0,0 +1,23 @@
+﻿string text = "Isso é um texto";
+
+Console.WriteLine(text.Length); // 16
+Console.WriteLine("Upper: " + text.ToUpper()); // ISSO É UM TEXTO
+Console.WriteLine("Lower: " + text.ToLower()); // isso é um texto
+
+Console.WriteLine("Replace: " + text.Replace("texto", "textinho")); // Isso é um textinho
+Console.WriteLine("Substring: " + text.Substring(5)); // é um texto
+
+Console.WriteLine("Split: " + text.Split(' ')[0]); // Isso
+
+Console.WriteLine("StartsWith: " + text.StartsWith("Isso")); // True
+Console.WriteLine("EndsWith: " + text.EndsWith("texto")); // True
+Console.WriteLine("Contains: " + text.Contains("texto")); // True
+
+string textoComeEspaco = "   Texto com espaços   ";
+Console.WriteLine("Trim: " + textoComeEspaco.Trim()); // Texto com espaços
+
+string texto1 = "Olá";
+string texto2 = "Olá";
+
+Console.WriteLine("Equals: " + texto1.Equals(texto2)); // True
+Console.WriteLine("Equals (=): "  + (texto1 == texto2)); // True
